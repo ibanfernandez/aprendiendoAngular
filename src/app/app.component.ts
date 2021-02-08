3a9d229e96b50354cbfb03754a8a1dd2d9c8702c
Iban Fernández <ibanfernandezz@gmai.com>
2021-02-08 13:02:52 +0100

    ng templates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Configuracion } from "./models/configuracion";
+import { Router, ActivatedRoute, Params } from "@angular/router";
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,16 +13,22 @@ export class AppComponent {
   public mostrar_videojuegos: boolean = true;
   public config;
 
-  constructor() {
+  constructor(
+    private _route: ActivatedRoute,
+    private _router: Router
+    ) {
     this.config = Configuracion;
     this.title = Configuracion.titulo;
     this.descripcion = Configuracion.descripcion;
-    
+
+  }
+  ngOnInit(): void {
+    this._router.navigate(['/home']);
   }
 
-  ocultarVideojuegos(value:boolean){
+  ocultarVideojuegos(value: boolean) {
     this.mostrar_videojuegos = value;
   }
-  
+
 }
 
